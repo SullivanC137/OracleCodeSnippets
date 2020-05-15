35f8bc718b12284ec25132ab110cc731a49bf750
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2020-05-15 09:39:56 +0200

    Update getCurrentPackageAndSubprogram.sql
    Added comments

--- getCurrentPackageAndSubprogram.sql
+++ getCurrentPackageAndSubprogram.sql
@@ -1,1 +1,2 @@
+-- use this to get the package and procedure name without hard coding
 l_module      varchar2(60) := substr(utl_call_stack.concatenate_subprogram(utl_call_stack.subprogram(1)),1,60);
