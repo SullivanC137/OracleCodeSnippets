585fc9f91dbf0b360974a14c310a0864da09fc84
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2021-08-05 08:29:48 +0200

    Update getDbmsOutputLines.sql

--- getDbmsOutputLines.sql
+++ getDbmsOutputLines.sql
@@ -16,13 +16,13 @@ declare
     end get_dbms_output;
 begin
     dbms_output.enable;
-    DBMS_OUTPUT.PUT_LINE('line 1');
-    DBMS_OUTPUT.PUT_LINE('line 2');
-    DBMS_OUTPUT.PUT_LINE('line 3');
-    DBMS_OUTPUT.PUT_LINE('line 4');
+    DBMS_OUTPUT.PUT_LINE('first line');
+    DBMS_OUTPUT.PUT_LINE('second line');
+    DBMS_OUTPUT.PUT_LINE('third line');
+    DBMS_OUTPUT.PUT_LINE('fourth line');
     l_test_arr := get_dbms_output();
     for i in l_test_arr.first .. l_test_arr.last loop
-    l_test := l_test||' - '||l_test_arr(i);
+    l_test := l_test||chr(10)||i||'. '||l_test_arr(i);
     end loop;
     DBMS_OUTPUT.PUT_LINE(l_test);
 end;
