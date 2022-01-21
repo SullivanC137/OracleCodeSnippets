d576b28e8cbb400f2e4939b06d9f5ebbe5bd551f
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2022-01-21 09:32:29 +0100

    Update utils.sql

--- utils.sql
+++ utils.sql
@@ -3,3 +3,7 @@ select listagg(rle_id, ':') within group (order by rle_id)
 from aut_usr_rle
 where usr_id = :USR_ID
 ;
+
+-- string to rows
+select column_value
+from   table(apex_string.split(:USR_RLE,':')
