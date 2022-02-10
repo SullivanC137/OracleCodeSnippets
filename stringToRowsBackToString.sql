a65b7d37616de3beda0869c4bffe8eae5b6ed5ad
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2022-02-10 15:54:39 +0100

    Update stringToRowsBackToString.sql

--- stringToRowsBackToString.sql
+++ stringToRowsBackToString.sql
@@ -1,15 +1,9 @@
--- rows to string
-select listagg(rle_id, ':') within group (order by rle_id) 
-from aut_usr_rle
-where usr_id = :USR_ID
-;
-
 -- string to rows APEX installed
 select column_value
 from   table(apex_string.split(:USR_RLE,':')
 ;
 
--- pure SQL, no APEX
+-- pure SQL, no APEX. String to rows
 with temp as
 (
     select 108 Name, 'test' Project, 'Err1, Err2, Err3' Error  from dual
@@ -23,4 +17,10 @@ from
   temp t,
   table(cast(multiset(select level from dual connect by  level <= length (regexp_replace(t.error, '[^,]+'))  + 1) as sys.OdciNumberList)) levels
 order by name
+;
+
+-- rows to string
+select listagg(rle_id, ':') within group (order by rle_id) 
+from aut_usr_rle
+where usr_id = :USR_ID
 ;
