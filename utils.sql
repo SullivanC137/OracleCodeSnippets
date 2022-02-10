82dcab94ae7a241eb14a118b00da14b11579b6b4
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2022-02-10 15:53:22 +0100

    Update utils.sql

--- utils.sql
+++ utils.sql
@@ -4,6 +4,23 @@ from aut_usr_rle
 where usr_id = :USR_ID
 ;
 
--- string to rows
+-- string to rows APEX installed
 select column_value
 from   table(apex_string.split(:USR_RLE,':')
+;
+
+-- pure SQL, no APEX
+with temp as
+(
+    select 108 Name, 'test' Project, 'Err1, Err2, Err3' Error  from dual
+    union all
+    select 109, 'test2', 'Err1' from dual
+)
+select distinct
+  t.name, t.project,
+  trim(regexp_substr(t.error, '[^,]+', 1, levels.column_value))  as error
+from 
+  temp t,
+  table(cast(multiset(select level from dual connect by  level <= length (regexp_replace(t.error, '[^,]+'))  + 1) as sys.OdciNumberList)) levels
+order by name
+;
