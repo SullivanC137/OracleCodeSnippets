e09cc834669ba0113d3a749657ae3d05b1406e22
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2022-02-11 08:21:10 +0100

    Delete csvToRows.sql

--- csvToRows.sql
+++
@@ -1,7 +0,0 @@
-with
-    q_str as (select 'YOUR:COMMA:SEPARATED:STRING' str from dual)
-select
-    regexp_substr(str, '[^:]+', 1, level) cee_id 
-from q_str
-connect by level <= regexp_count(str, ':') + 1
-;
