0fdc649041b1bff37bbdc4e06cc8d701544384ab
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2022-02-11 08:21:58 +0100

    Update stringToRowsBackToString.sql

--- stringToRowsBackToString.sql
+++ stringToRowsBackToString.sql
@@ -19,6 +19,15 @@ from
 order by name
 ;
 
+-- shorter way, again string to rows
+with
+    q_str as (select 'YOUR:COMMA:SEPARATED:STRING' str from dual)
+select
+    regexp_substr(str, '[^:]+', 1, level) cee_id 
+from q_str
+connect by level <= regexp_count(str, ':') + 1
+;
+
 -- rows to string
 select listagg(rle_id, ':') within group (order by rle_id) 
 from aut_usr_rle
