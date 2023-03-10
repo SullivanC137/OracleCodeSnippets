77f23fcfe723705003e21180d578b25736d06b5a
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2023-03-10 10:32:30 +0100

    Create create_file_with_filenames.cmd
    handy for creating a script that calls the folder contents.

---
+++ create_file_with_filenames.cmd
@@ -0,0 +1,5 @@
+REM create a file with all filenames
+REM for easy navigation you can navigate with windows explorer and anter "cmd" into the address bar
+REM navigate to your folder
+REM use:
+dir /b > filenames.txt
