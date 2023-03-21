7441a16472fc4da458ea0bf2ac9ac92ecb1b111e
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2023-03-21 14:27:58 +0100

    Create validateConversion.sql

---
+++ validateConversion.sql
@@ -0,0 +1,11 @@
+-- use to check which records would not be able to be converted to a date
+WITH test1(Customer_DOB) AS (
+    SELECT '22-JAN-87' FROM dual UNION ALL
+    SELECT '11-Nov-81' FROM dual UNION ALL
+    SELECT '15-06-75'  FROM dual UNION ALL
+    SELECT '22-JUN-56' FROM dual
+)
+SELECT *
+FROM   test1
+WHERE  validate_conversion(Customer_DOB AS DATE, 'DD-Mon-RR') = 0
+;
