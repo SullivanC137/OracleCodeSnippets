2b11c2363c9c57e7031af33e3be2989005ffe2af
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2023-06-14 08:47:24 +0200

    Delete createFileWithFileNames.cmd

--- createFileWithFileNames.cmd
+++
@@ -1,5 +0,0 @@
-REM create a file with all filenames
-REM for easy navigation you can navigate with windows explorer and anter "cmd" into the address bar
-REM navigate to your folder
-REM use:
-dir /b > filenames.txt
