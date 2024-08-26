b73aa0610d001b0e3bd5daba66ede257ab6204e0
Sullivan Kromosoeto <sullivan.kromosoeto@outlook.com>
2024-08-26 14:26:06 +0200

    Update apexDownloadFile.sql
    added better variable names

--- apexDownloadFile.sql
+++ apexDownloadFile.sql
@@ -4,24 +4,27 @@ Place this code in a process before header. This example gets data from a packag
 */
 
 declare  
-  l_file_clob       clob;
-  l_file_blob       blob;
   l_extension       varchar2(10);
-  o1 integer;
-  o2 integer;
-  c integer;
-  w integer;
+  l_file_clob    clob;
+  l_file_blob    blob;
+  l_dest_offset  integer := 1;
+  l_src_offset   integer := 1;
+  l_lang_context integer := 0;
+  l_warning      integer := 0;
 begin  
   --create file content
   l_file_clob := some_kind_of_package.get_file_contents; 
   l_extension := '.sql';
   -- convert to blob
-  o1 := 1;
-  o2 := 1;
-  c := 0;
-  w := 0;
   DBMS_LOB.CreateTemporary(l_file_blob, true);
-  DBMS_LOB.ConvertToBlob(l_file_blob, l_file_clob, length(l_file_clob), o1, o2, 0, c, w);
+  dbms_lob.converttoblob(dest_lob     => l_file_blob
+                      ,src_clob     => l_file_clob
+                      ,amount       => length(l_file_clob)
+                      ,dest_offset  => l_dest_offset
+                      ,src_offset   => l_src_offset
+                      ,blob_csid    => 0
+                      ,lang_context => l_lang_context
+                      ,warning      => l_warning);
   --download file  
   htp.init;  
   owa_util.mime_header(  
